7906d5049de5fac848c2eb4794a2e028fe85e236
Danny-Erikson <derikson413@gmail.com>
2025-10-29 10:13:57 -0700

    Setup main loop for project

--- main.py
+++ main.py
@@ -3,9 +3,20 @@
 # throughout this file
 import pygame
 
+# constants from constants.py
+import constants as const
+
 
 def main():
     print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
