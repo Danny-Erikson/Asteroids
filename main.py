8117bb43c30cdb742e08457a27218c6570d6b844
Danny-Erikson <derikson413@gmail.com>
2025-11-04 09:22:42 -0800

    Set up player

--- main.py
+++ main.py
@@ -4,19 +4,44 @@
 import pygame
 
 # constants from constants.py
-import constants as const
+from constants import *
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
-    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Lock fps so it does speed up or slow down with CPU speed
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
+        # Handler for quitting
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        player.draw(screen)
         pygame.display.flip()
+
+        # Locks the game to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
